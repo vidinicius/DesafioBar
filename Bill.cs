205e1e04532d4e7badf4683aa434f632f8b3bae3
vforn <vforn@DESKTOP-8F6MR4L>
2025-03-27 21:43:04 -0300

    Add project files.

---
+++ Bill.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioBar
+{
+    class Bill
+    {
+        public char Gender;
+        public int Beer;
+        public int Barbecue;
+        public int SoftDrink;
+
+        public double Cover()
+        {
+            if (Feeding() > 30)
+            {
+                return 0;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+
+        public double Feeding()
+        {
+            return Beer * 5 + SoftDrink * 3 + Barbecue * 7;
+
+        }
+        public double Ticket()
+        {
+            if(Gender == 'F' || Gender == 'f')
+            {
+                return 8;
+            }
+            else
+            {
+                return 10;
+            }
+
+        }
+
+        public double Total()
+        {
+            return Cover() + Feeding() + Ticket();
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace DesafioBar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bill conta = new Bill();
+            
+            Console.Write("Sexo: ");
+            conta.Gender = char.Parse(Console.ReadLine());
+            
+            Console.Write("Quantidade de cervejas: ");
+            conta.Beer = int.Parse(Console.ReadLine());
+                        
+            Console.Write("Quantidade de refrigerantes: ");
+            conta.SoftDrink = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de espetinhos: ");
+            conta.Barbecue = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\nRelatório: ");
+            Console.WriteLine("Consumo = R$ " + conta.Feeding().ToString("F2",CultureInfo.InvariantCulture));
+            
+            if (conta.Cover() > 0)
+            {
+                Console.WriteLine("Covert = R$ " + conta.Cover().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("Isento de couvert!");
+            }    
+                
+            Console.WriteLine("Ingresso = R$ " + conta.Ticket().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("\nValor a pagar = R$ " + conta.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
